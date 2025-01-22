0f1447da144c48761a56e7933c263c6fb8769b95
Manoj R Ponagandla <mponagandla@gmail.com>
2025-01-22 15:36:44 -0600

    added webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+
+module.exports = {
+    // Other configurations...
+    resolve: {
+        fallback: {
+            "crypto": require.resolve("crypto-browserify"),
+            "fs": false
+        }
+    },
+    // Other configurations...
+};
