b3d2d4717dfcc5db6eab17a3167261491418896b
Manoj R Ponagandla <mponagandla@gmail.com>
2025-01-22 17:24:22 -0600

    dependency changes

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,8 @@ module.exports = {
         fallback: {
             "crypto": require.resolve("crypto-browserify"),
             "stream": require.resolve("stream-browserify"),
+            "timers": require.resolve("timers-browserify"),
+            "zlib": require.resolve("browserify-zlib"),
             "fs": false
         }
     }
