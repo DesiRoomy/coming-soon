336055df2ec5b1388e68d7ff76129612a81b7c03
Manoj R Ponagandla <mponagandla@gmail.com>
2025-01-22 16:23:31 -0600

    dependency changes

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,33 @@
 const path = require('path');
 
 module.exports = {
-    // Other configurations...
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
     resolve: {
         fallback: {
             "crypto": require.resolve("crypto-browserify"),
+            "stream": require.resolve("stream-browserify"),
             "fs": false
         }
     },
-    // Other configurations...
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env', '@babel/preset-react']
+                    }
+                }
+            }
+        ]
+    },
+    plugins: [
+        // Add any necessary plugins here
+    ]
 };
